fd06f8ac3dcab49cd9e9e87d0cbd813e1fd22c10
JACK-QBS <2200838299@qq.com>
2020-11-16 21:06:29 +0800

    用户属性 (JavaBean)

---
+++ 购物车/user.java
@@ -0,0 +1,41 @@
+package database;
+
+public class user {
+
+    private String name;
+    private String password;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
