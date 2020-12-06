dfb2302f064dea3b8492a58c17dc9ac29400c7ce
JACK-QBS <2200838299@qq.com>
2020-12-06 15:15:42 +0800

    跳转到商品展示页面

---
+++ 购物车 2.0/后端处理/购物车的增删查改/ShoppingServlet.java
@@ -0,0 +1,21 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ShoppingServlet",urlPatterns = "/shopping")
+public class ShoppingServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request,response);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/css/goods.jsp").forward(request,response);
+    }
+}
