1c8b404c0f9ea405047565acf5871100016478e4
JACK-QBS <2200838299@qq.com>
2021-01-09 20:57:26 +0800

    PostFileServlet.java

---
+++ 习题练习/PostFileServlet.java
@@ -0,0 +1,20 @@
+package com.qbs.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/post-message.api")
+public class PostFileServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        req.setCharacterEncoding("utf-8");
+
+        System.out.println(req.getParameter("username"));
+        System.out.println(req.getParameter("password"));
+    }
+}
